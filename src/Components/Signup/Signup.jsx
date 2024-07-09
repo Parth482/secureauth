2c9bd3f38968e9edc5f026503cb749f246c2f9e4
Parth Joshi <138760739+Parth482@users.noreply.github.com>
2024-07-09 19:04:37 +0530

    Update Signup.jsx

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -21,7 +21,7 @@ const Signup = ({ setAction }) => {
   const handleSignUp = async () => {
     try {
       //signup logic...
-      const response = await axios.post('//3.109.122.70:3000/auth/signup', {
+      const response = await axios.post('http://localhost:3000/auth/signup', {
         username,
         email,
         password,
@@ -40,7 +40,7 @@ const Signup = ({ setAction }) => {
   const handleVerifyOtp = async () => {
     try {
       // Verify OTP
-      const response = await axios.post('//3.109.122.70:3000/auth/verify-otp', { email, otp });
+      const response = await axios.post('http://localhost:3000/auth/verify-otp', { email, otp });
       console.log('Sending OTP verification request with data:', { email, otp });
       // If OTP is verified, close the popup and proceed to the dashboard
       setShowOtpPopup(false);
